81b5e4cabf0645d2668f0223e98fbf1370ff8982
Oksana Mosendz <bagheera03@gmail.com>
2026-03-30 12:06:40 -0500

    add core-modules,async and globals- demo scripts

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,75 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
+const samplePath = path.join(__dirname, "sample-files", "sample.txt");
 
 // Write a sample file for demonstration
 
+const createSampleFile = async () => {
+  try {
+    await fs.promises.writeFile(samplePath, "Hello, async world!");
+  } catch (err) {
+    console.log("Create sample error:", err);
+  }
+};
+
+createSampleFile();
+
 // 1. Callback style
+fs.readFile(samplePath, "utf8", (err, data) => {
+  if (err) {
+    console.log("Callback read error:", err.message);
+  } else {
+    console.log("Callback read:", data);
+  }
+});
 
+// Callback hell example (test and leave it in comments):
+// fs.readFile( path.resolve('sample-files', 'sample1.txt'),'utf8', (err, data1)=>{
+// if(err){
+//   console.log('file read error:', err.message)
+// }
+//   console.log('Callback read:', data1)
+// })
 
-  // Callback hell example (test and leave it in comments):
+// fs.readFile( path.resolve('sample-files', 'sample2.txt'),'utf8', (err, data2)=>{
+// if(err){
+//   console.log('file read error:', err.message)
+// }
+//   console.log('Callback read:', data2)
+// })
 
+// fs.readFile( path.resolve('sample-files', 'sample3.txt'),'utf8', (err, data3)=>{
+// if(err){
+//   console.log('file read error:', err.message)
+// }
+//   console.log('Callback read:', data3)
+// })
 
-  // 2. Promise style
+// 2. Promise style
+const readFile1 = async (path) => {
+  try {
+    const data = await new Promise((resolve, reject) => {
+      fs.readFile(path, "utf8", (err, data) => {
+        return err ? reject(err) : resolve(data);
+      });
+    });
+    console.log("Promise read:", data);
+  } catch (err) {
+    console.log("Promise read error:", err.message);
+  }
+};
 
+readFile1(samplePath);
 
-      // 3. Async/Await style
+// 3. Async/Await style
+
+const readFile2 = async (path) => {
+  try {
+    const data = await fs.promises.readFile(path, "utf8");
+    console.log("Async/Await read:", data);
+  } catch (err) {
+    console.log("Async/Await read error:", err.message);
+  }
+};
+readFile2(samplePath);
--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,18 +1,70 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
 
-const sampleFilesDir = path.join(__dirname, 'sample-files');
+const sampleFilesDir = path.join(__dirname, "sample-files");
 if (!fs.existsSync(sampleFilesDir)) {
   fs.mkdirSync(sampleFilesDir, { recursive: true });
 }
 
 // OS module
-
+console.log("Platform:", os.platform());
+console.log("CPU:", os.cpus()[1].model);
+console.log("Total Memory:", os.totalmem());
 
 // Path module
+const joinedPath = path.join(__dirname, "sample-files/folder", "file.txt");
+console.log("Joined path:", joinedPath);
 
 // fs.promises API
 
+const creatAndReadFile = async () => {
+  try {
+    const demoPath = path.join(sampleFilesDir, "demo.txt");
+    await fs.promises.writeFile(demoPath, "Hello from fs.promises!");
+    const data = await fs.promises.readFile(demoPath, "utf8");
+    console.log("fs.promises read:", data);
+  } catch (err) {
+    console.log("fs.promises error:", err);
+  }
+};
+
+creatAndReadFile();
 
 // Streams for large files- log first 40 chars of each chunk
+
+const largeFilePath = path.join(sampleFilesDir, "largefile.txt");
+
+const writeFile = function () {
+  const writeStream = fs.createWriteStream(largeFilePath);
+  for (let i = 1; i < 101; i++) {
+    writeStream.write(` This is ${i} line of this large file\n`);
+  }
+  writeStream.end();
+
+  writeStream.on("error", (err) => {
+    console.error("Error writing file:", err);
+  });
+};
+
+const readFile = function () {
+  const readStream = fs.createReadStream(largeFilePath, {
+    encoding: "utf8",
+    highWaterMark: 1024,
+  });
+
+  readStream.on("data", (chunk) => {
+    console.log("Read chunk:", chunk.slice(0, 10));
+  });
+
+  readStream.on("end", () => {
+    console.log("Finished reading large file with streams.");
+  });
+
+  readStream.on("error", (err) => {
+    console.error("Error reading file:", err);
+  });
+};
+
+writeFile();
+readFile();
