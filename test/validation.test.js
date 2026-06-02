42e31a0b193e59a2d03eea911697ef33461ed3fd
Oksana Mosendz <bagheera03@gmail.com>
2026-06-02 12:55:12 -0500

    create taests 2-7 for userschema

--- test/validation.test.js
+++ test/validation.test.js
@@ -11,4 +11,66 @@ describe("user object validation tests", () => {
       error.details.find((detail) => detail.context.key == "password"),
     ).toBeDefined();
   });
-});
+
+  it("2. an email is specified",()=>{
+const { error } = userSchema.validate(
+      { name: "Bob", password: "!1Password" },
+      { abortEarly: false },
+    );
+       expect(
+      error.details.find((detail) => detail.context.key == "email"),
+    ).toBeDefined();
+  });
+
+  
+  it("3. doesn't accept invalid email",()=>{
+const { error } = userSchema.validate(
+      { name: "Bob", email: "bobsamplecom", password: "!1Password" },
+      { abortEarly: false },
+    );
+       expect(
+      error.details.find((detail) => detail.context.key == "email"),
+    ).toBeDefined();
+  });
+
+  it("4. password is required",()=>{
+const { error } = userSchema.validate(
+      { name: "Bob", email: "bob@sample.com"},
+      { abortEarly: false },
+    );
+       expect(
+      error.details.find((detail) => detail.context.key == "password"),
+    ).toBeDefined();
+  });
+
+  it("5. name is required",()=>{
+const { error } = userSchema.validate(
+      {  email: "bob@sample.com", password: "!1Password" },
+      { abortEarly: false },
+    );
+       expect(
+      error.details.find((detail) => detail.context.key == "name"),
+    ).toBeDefined();
+  });
+
+    it("6. should accept valid name",()=>{
+const { error } = userSchema.validate(
+      { name: "Bo", email: "bob@sample.com", password: "!1Password" },
+      { abortEarly: false },
+    );
+       expect(
+      error.details.find((detail) => detail.context.key == "name"),
+    ).toBeDefined();
+  });
+
+  
+    it("7. error comes back falsy, when validation is performed on a valid user object", ()=>{
+const { error } = userSchema.validate(
+      { name: "Bob", email: "bob@sample.com", password: "!1Password" },
+      { abortEarly: false },
+    );
+       expect(error).toBeFalsy();
+  });
+
+
+  })
