16079a631822d634b817f2c6606f83599e6da9c7
Oksana Mosendz <bagheera03@gmail.com>
2026-06-08 19:27:21 -0500

    fix paginationSchema

--- validation/paginationSchema.js
+++ validation/paginationSchema.js
@@ -2,7 +2,25 @@ const Joi = require("joi");
 
 const paginationSchema = Joi.object({
   page: Joi.number().integer().min(1).default(1),
-  limit: Joi.number().integer().min(1).max(100).default(10)
+
+  limit: Joi.number().integer().min(1).default(10),
+
+  sortBy: Joi.string()
+    .valid("title", "priority", "createdAt", "id", "isCompleted")
+    .default("createdAt"),
+
+  sortDirection: Joi.string()
+    .valid("asc", "desc")
+    .default("desc"),
+
+  find: Joi.string().allow(""),
+  isCompleted: Joi.string(),
+  priority: Joi.string(),
+  min_date: Joi.date(),
+  max_date: Joi.date(),
+}).options({
+  allowUnknown: true,
+  convert: true,
 });
 
-module.exports={paginationSchema}
+module.exports={paginationSchema};
